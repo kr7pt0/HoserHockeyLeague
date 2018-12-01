47ccef1308bb5388b809822ed10619b8e9604fc8
naldokan <oscar.endo318@gmail.com>
2018-12-01 12:33:29 -0800

    Menu Icon 1.) Create three line menu icon using before and after pseudo elements.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,19 +7,24 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <div className="container">
           <nav>
-            <Link to='/'>
-            <div className="logo"></div>
-            </Link>
-            <ul>
-              <Link to='/news' className="nav-link"><li>News</li></Link>
-              <Link to='/rules' className="nav-link"><li>Rules</li></Link>
-              <Link to='/gms' className="nav-link"><li>GMS</li></Link>
-              <Link to='/awards' className="nav-link"><li>Awards</li></Link>
-            </ul>
+            <div className="navbar">
+              <Link to='/'>
+              <div className="logo"></div>
+              </Link>
+
+              <div className="nav-icon"></div>
+            </div>
+
+            <div className="nav-menu">
+              <ul>
+                <Link to='/news' className="nav-link"><li>News</li></Link>
+                <Link to='/rules' className="nav-link"><li>Rules</li></Link>
+                <Link to='/gms' className="nav-link"><li>GMS</li></Link>
+                <Link to='/awards' className="nav-link"><li>Awards</li></Link>
+              </ul>
+            </div>
           </nav>
-        </div>
       </header>
     );
   }
