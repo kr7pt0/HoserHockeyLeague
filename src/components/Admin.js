059ea75d1521e18adb312e8efb4db5b189bcc5ed
naldokan <oscar.endo318@gmail.com>
2019-01-12 18:11:18 -0800

    Removed commented code.

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -44,29 +44,6 @@ class Admin extends React.Component {
       })
     }
 
-      // heresabutton(e){
-      //   e.preventDefault();
-      //   console.log('clicked');
-      //   this.setState({teststate: {
-      //     fish1: {
-      //       name: 'Pacific Halibut',
-      //       image: 'https://i.istockimg.com/file_thumbview_approve/36248396/5/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
-      //       desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
-      //       price: 1724,
-      //       status: 'available'
-      //     },
-      //
-      //     fish2: {
-      //       name: 'Lobster',
-      //       image: 'https://i.istockimg.com/file_thumbview_approve/32135274/5/stock-photo-32135274-cooked-lobster.jpg',
-      //       desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
-      //       price: 3200,
-      //       status: 'available'
-      //     }
-      //   }
-      // })
-      // }
-
       submitRecap(e){
         e.preventDefault()
         console.log("clicked");
