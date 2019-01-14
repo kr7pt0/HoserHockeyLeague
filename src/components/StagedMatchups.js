bce44c51bd0a38048efef3b807163cf951049059
naldokan <oscar.endo318@gmail.com>
2019-01-13 18:56:46 -0800

    Edit Matchup Recap Component 1) Reformat component state 2) StagedMatchup component edit button passing data to MatchupRecap component

--- src/components/StagedMatchups.js
+++ src/components/StagedMatchups.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
 class StagedMatchups extends React.Component {
 
 
   renderStages(key){
     const staged = this.props.staged[key];
     // console.log(staged , 'staged');
+    const liStyle = {width: '100%', border: '1px solid black'}
 
     return (
-      <li key={key}>
-        <span>{staged.homeTeam} vs {staged.awayTeam}</span>
+      <li key={key} style={liStyle}>
+        {staged.homeTeam} vs {staged.awayTeam} | <span onClick={(e) => {this.props.setMatchupRecap(staged)}}>edit</span>
       </li>
     )
   }
@@ -19,10 +19,12 @@ class StagedMatchups extends React.Component {
     const h1style = {fontSize: '40px'}
     const stagedIds = Object.keys(this.props.staged)
     // console.log(stagedIds, "stagedIds");
+    const ulStyle = {width: '400px', background: 'yellow'}
+
     return (
       <div className="staged-matchups" style={divStyle}>
         <h1 style={h1style}>StagedMatchups</h1>
-        <ul>
+        <ul style={ulStyle}>
           {stagedIds.map(this.renderStages.bind(this))}
         </ul>
       </div>
