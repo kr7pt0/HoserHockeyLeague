e36871610a9f0773a52f929c26efe3e1b39a39e6
naldokan <oscar.endo318@gmail.com>
2019-02-15 01:14:44 -0800

    Add popup for edit displayName/email/password

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router';
 import logo from '../img/logo.png';
-import { rebaseAuth } from '../config';
 
 class Header extends React.Component {
 
+  constructor(){
+    super();
+
+    this.state = {
+      popupOpen: false
+    }
+
+    this.togglePopup = this.togglePopup.bind(this);
+  }
+
+
+  togglePopup(e){
+    console.log('popup fired');
+    if(e && (e.target.nodeName === 'FORM' || e.target.nodeName === 'INPUT' || e.target.nodeName === 'BUTTON')){
+      console.log('not closing');
+    } else {
+      this.setState({popupOpen: !this.state.popupOpen})
+    }
+  }
+
   adminDashboard(){
+    console.log(this.state.popupOpen, 'popup');
+    let popupStyle = {
+      position: 'fixed',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      background: 'rgba(0,0,0,0.5)',
+      display: 'none',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+
+    const formStyle = {
+      'zIndex': 100,
+      background: 'pink',
+      position: 'absolute'
+    }
+
+    this.state.popupOpen ? popupStyle.display = 'flex' : '';
+
     if(this.props.admin.loggedIn){
       return (
         <div className="admin-dashboard">
@@ -14,16 +54,29 @@ class Header extends React.Component {
               <img src="http://placehold.it/50x50" alt="User" />
               <span>Welcome, {this.props.admin.adminUser.displayName ? this.props.admin.adminUser.displayName : this.props.admin.adminUser.email}</span>
             </Link>
+            <p style={{color: 'white'}} onClick={()=>{this.togglePopup()}}>asdasgsa</p>
           </div>
+
           <Link to='/'><button onClick={this.props.logout}>logout</button></Link>
+
+
+          { /* POPUP FOR ON CLICK SETTINGS */ }
+
+          <div className="popup"  style={popupStyle} onClick={(e)=>{this.togglePopup(e)}}>
+            <p onClick={()=>{this.togglePopup()}}>XXXX</p>
+            <form style={formStyle}>
+              <input type="text" placeholder='Display Name'/>
+              <input type="text" placeholder='Update Email'/>
+              <input type="text" placeholder='Update Password'/>
+              <button>Submit</button>
+            </form>
+          </div>
         </div>
       )
     }
   }
 
   render() {
-    console.log(this.props);
-    console.log(rebaseAuth.currentUser, 'currentUser in header');
     return(
       <div className="header">
         <div className="logo">
